fe3abdfe57a2c930bde78177d6954246c177c9b5
SothanaV <nai6320@gmail.com>
2021-03-30 23:27:01 +0700

    [ADD] api upload

--- backend/api/v1/routers.py
+++ backend/api/v1/routers.py
@@ -1,3 +1,5 @@
 from rest_framework import routers
+from .viewsets import UploadViewset
 
 router = routers.DefaultRouter()
+router.register('upload', UploadViewset, basename='upload')
--- backend/api/v1/viewsets.py
+++ backend/api/v1/viewsets.py
@@ -1,1 +1,10 @@
 from rest_framework import viewsets
+from .serializers import UploadSerializer
+from upload.models import Upload
+
+class UploadViewset(viewsets.ModelViewSet):
+    serializer_class = UploadSerializer
+    model = Upload
+
+    def get_queryset(self):
+        return self.model.objects.none()
